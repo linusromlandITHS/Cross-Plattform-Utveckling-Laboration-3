74656923047bb539bf43e5613a220cb54162f5b1
Linus Romland <romlandlinus@gmail.com>
2022-06-10 08:12:50 +0200

    Init `react-navigation`

---
+++ app/screens/Home.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native';
+
+export default () => {
+	return (
+		<View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+			<Text>Welcome to Ferry Departures</Text>
+		</View>
+	);
+};
