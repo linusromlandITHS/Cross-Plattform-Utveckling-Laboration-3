233e3639cbccda38b619821fb80c84f7fb3d1acd
Linus Romland <romlandlinus@gmail.com>
2022-06-10 18:15:26 +0200

    Moved router to seperate file

--- app/App.tsx
+++ app/App.tsx
@@ -1,25 +1,8 @@
-//External dependencies
-import * as React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-
 //Internal dependencies
-import { HomeScreen, RouteScreen } from './screens';
-
-const Stack = createNativeStackNavigator();
+import Router from './Router';
 
 function App() {
-	return (
-		<NavigationContainer>
-			<Stack.Navigator
-				screenOptions={{
-					headerShown: false
-				}}>
-				<Stack.Screen name='Home' component={HomeScreen} />
-				<Stack.Screen name='Route' component={RouteScreen} />
-			</Stack.Navigator>
-		</NavigationContainer>
-	);
+	return <Router />;
 }
 
 export default App;
